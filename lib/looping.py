3477e73628c11f18d7b3e51f583e7bf4132289b3
Marvin <marvin.ogot@gmail.com>
2024-03-06 21:42:40 +0300

    Working with for and while loops

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,29 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    num = 10
+    while  num > 0:
+        print(num)
+        num -= 1
+    print("Happy New Year!")
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    squared_list = []
+    for int in int_list:
+        square = int * int
+        squared_list.append(square)
+    return squared_list
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for i in range(1,101):
+        if (i % 3 == 0 and i % 5 == 0):
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(f"{i}")
+
+
+
